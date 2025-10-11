a9ceef86292102e23b2624e66355710c34575d9f
ShashwatTripathi998 <shashwattripathi874@gmail.com>
2025-10-11 10:09:07 +0000

    A, B ,C , D Grading system

---
+++ q18.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main()
+{
+    int marks;
+    printf("enter your marks: ");
+    scanf("%d",&marks);
+    if(marks>=90)
+    {
+        printf("A Grade\n");
+    }
+    else if(marks>=70)
+    {
+        printf("B Grade\n");
+    }
+    else if(marks>=50)
+    {
+        printf("C Grade\n");
+    }
+    else if(marks>=33)
+    {
+        printf("D Grade\n");
+    }
+    else if(marks<33)
+    {
+        printf("Failed\n");
+    }
+    return 0;
+}
