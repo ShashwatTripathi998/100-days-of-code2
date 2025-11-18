4f3f8d24159ca8db1d00b6ca9260225f9f376c19
ShashwatTripathi998 <shashwattripathi874@gmail.com>
2025-11-18 17:46:49 +0000

    perfect number

---
+++ q42.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main() {
+    int num, i, sum = 0;
+
+    printf("Enter a positive integer: ");
+    scanf("%d", &num);
+
+    if (num <= 0) {
+        printf("Please enter a positive integer.\n");
+        return 1;
+    }
+    // Find all proper divisors and sum them
+    for (i = 1; i < num; i++) {
+        if (num % i == 0) {
+            sum += i;
+        }
+    }
+    // Check if sum of divisors equals the number
+    if (sum == num)
+        printf("%d is a perfect number.\n", num);
+    else
+        printf("%d is not a perfect number.\n", num);
+
+    return 0;
+}
