2fd35644fc175216dd3c737a193cf17415586425
ShashwatTripathi998 <shashwattripathi874@gmail.com>
2025-11-26 05:19:46 +0000

    Rotating elements of array

---
+++ q70.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+void reverse(int arr[], int start, int end) {
+    while (start < end) {
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+        start++;
+        end--;
+    }
+}
+void rotateRight(int arr[], int n, int k) {
+    k = k % n;
+    reverse(arr, 0, n - 1);
+    reverse(arr, 0, k - 1);
+    reverse(arr, k, n - 1);
+}
+int main() {
+    int n, k, i;
+    printf("Enter the number of elements in the array: ");
+    scanf("%d", &n);
+    int arr[n];
+    printf("Enter %d elements:\n", n);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    printf("Enter the number of positions to rotate: ");
+    scanf("%d", &k);
+    rotateRight(arr, n, k);
+    printf("Array after rotating to the right by %d positions:\n", k);
+    for (i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+    return 0;
+}
