f378de01b41b0415b62bc24e01bf0539130056d2
ShashwatTripathi998 <shashwattripathi874@gmail.com>
2025-11-26 05:49:06 +0000

    sum of elements of a matrix

---
+++ q72.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main() {
+    int rows, cols;
+    int i, j;
+    int sum = 0;
+    printf("Enter number of rows: ");
+    scanf("%d", &rows);
+    printf("Enter number of columns: ");
+    scanf("%d", &cols);
+    int matrix[rows][cols];
+    printf("Enter elements of the matrix:\n");
+    for (i = 0; i < rows; i++) {
+        for (j = 0; j < cols; j++) {
+            printf("Element [%d][%d]: ", i + 1, j + 1);
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+    for (i = 0; i < rows; i++) {
+        for (j = 0; j < cols; j++) {
+            sum += matrix[i][j];
+        }
+    }
+    printf("The sum of all elements in the matrix is: %d\n", sum);
+    return 0;
+}
