22597cc91f28a2c83a76bed786627f6b83f8af42
ShashwatTripathi998 <shashwattripathi874@gmail.com>
2025-11-27 16:56:41 +0000

    sub-string

---
+++ q100.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+int main() {
+    char str[100];
+    int i, j, k;
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+    str[strcspn(str, "\n")] = '\0';
+    int len = strlen(str);
+    printf("All substrings of \"%s\":\n", str);
+    for (i = 0; i < len; i++) {
+        for (j = i; j < len; j++) {
+            for (k = i; k <= j; k++) {
+                printf("%c", str[k]);
+            }
+            printf("\n");
+        }
+    }
+    return 0;
+}
